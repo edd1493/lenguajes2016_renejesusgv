b0f65cab53aa8c2f918b676259339599c4d7dba6
renejesusgv <renejesusgv@hotmail.com>
2015-09-03 00:39:42 -0500

    Test's terminados, pendientes primes y conjunto potencia

--- Practicas/Practica1/practica1.rkt
+++ Practicas/Practica1/practica1.rkt
@@ -1,8 +1,6 @@
 #lang plai
-#|(test <result-expr> <expected-expr>)|#
 
-
-
+#|Section 1|#
 
 (define (POW z w)
   (if (= w 0)
@@ -32,60 +30,88 @@
 (test (average '(3 5 10)) 6)
 (test (average '(10 10 10 10 10 10)) 10)
 (test (average '(0 6 4 0)) 2.5)
-#|4.zip - Dadas dos listas, regresar una cuyos elementos son listas de tamaño 2, tal que para la i-ésima lista, el primer
-elemento es el i-ésimo de la primera lista original, y el segundo elemento es el i-ésimo de la segunda lista original,
-si una lista es de menor tamaño que la otra, la lista resultante es del tamaño de la menor, y si una de las listas es vacía,
-regresa una lista vacía|#
-(define (zip l1 l2)
-  (if (or (empty? l1) (empty? l2))
+
+#|
+(test (primes 5) '(2 3 5))
+(test (primes 100) '(muchos primos))
+(test (primes 1000) '(muchos mas primos))
+(test (primes 0) '())
+(test (primes 2) '(2))|#
+
+(define (zip list1 list2)
+  (if (or (empty? list1) (empty? list2))
       empty
-      (cons (list (car l1) (car l2)) (zip (cdr l1) (cdr l2)))))
+      (cons (list (car list1) (car list2)) (zip (cdr list1) (cdr list2)))))
 
-#|5.reduce - Dada una función de aridad 2 y una lista de n elementos, regresar la evaluación de la funcion
-encadenada de todos los elementos|#
-(define reduce (lambda (f l)
-                 (if (eq? (cdr l) empty)
-                 (car l)
-                 (f (car l) (reduce f (cdr l))))))
+(test (zip '(0) '()) '())
+(test (zip '(0 6 4 0) '(4 5 3)) '((0 4)(6 5)(4 3)))
+(test (zip '(5) '(1 2 3 4 5 6 7)) '((5 1)))
+(test (zip '(a) '(a v c)) '((a a)))
+(test (zip '(a b c d) '(1 2)) '((a 1)(b 2)))
+
+(define reduce (lambda (function list)
+                 (if (eq? (cdr list) empty) (car list)
+                 (function (car list) (reduce function (cdr list))))))
+
+(test (reduce POW '(2 2 2)) 16)
+(test (reduce + '(2 5 3 6 7)) 23)
+(test (reduce + '(65)) 65)
+(test (reduce - '(4 5)) -1)
+(test (reduce * '(1 2 3 4 5)) 120)
 
 
+#|Section 2|#
 
-#|SECCIÓN 2|#
-
-#|1.mconcat - Dadas dos listas, regresa la concatenación de la primera con la segunda.|#
 (define (mconcat l1 l2)
   (if(empty? l1)l2
      (cons (car l1) (mconcat (cdr l1) l2))))
 
 (test (mconcat '(1234)'(2345))'(1234 2345))
 (test (mconcat '(123) '(2223)) '(123 2223))
-(test(mconcat '() '()) '())
-(test(mconcat '(1) '()) '(1))
+(test (mconcat '() '()) '())
+(test (mconcat '(1) '()) '(1))
 (test(mconcat '(2222222) '(333333)) '(2222222 333333))
 
 
-#|2.mmap - Dada una función de aridad 1 y una lista, regresar una lista con la aplcicación de la función a cada 
-uno de los elementos de la lista original|#
-(define (mmap f l)
-  (if(empty? l)empty
-     (cons (f (car l)) (mmap f (cdr l)))))
+(define (mmap function list)
+  (if(empty? list)empty
+     (cons (function (car list)) (mmap function (cdr list)))))
 
-#|3.mfilter - Dado un predicado de un argumento y una lista, regresa la lista original sin los elementos que al aplicar
-el predicado, regrese falso|#
-(define (mfilter p l)
+(test (mmap add1 '(3 4 2 3 4 5)) '(4 5 3 4 5 6))
+(test (mmap sub1 '(3 4 2 3 4 5)) '(2 3 1 2 3 4))
+(test (mmap car '((5 6 4 3))) '(5))
+(test (mmap cdr '((4 5 3 9 7 5 5))) '((5 3 9 7 5 5)))
+(test (mmap cdr '()) '())
+
+
+(define (mfilter predicate list)
   (cond
-    [(empty? l) empty]
-    [(eq? (p (car l)) #t) (cons (car l) (mfilter p (cdr l)))]
-    [else (mfilter p (cdr l))]))
-    
+    [(empty? list) empty]
+    [(eq? (predicate (car list)) #t) (cons (car list) (mfilter predicate (cdr list)))]
+    [else (mfilter predicate (cdr list))]))
 
-#|any and every |#
+(test (mfilter (lambda (x) (not (zero? x)))'(3 3 4 2 0 4 0 34 0)) '(3 3 4 2 4 34))
+(test (mfilter (lambda (list) (not (empty? list)))'((4 3 4) ())) '((4 3 4)))
+(test (mfilter (lambda (n) (zero? n)) '(0 0 0)) '(0 0 0))
+(test (mfilter (lambda (x) (not(= x 4)))'(1 2 3 4 5 6 7 8 9)) '(1 2 3 5 6 7 8 9))
+(test (mfilter (lambda (x) (negative? x))'(2 1 0 -1 -2 -3 -4 -5)) '(-1 -2 -3 -4 -5))
 
-(define (any? p? list)
+(define (any? predicate? list)
   (if (empty? list) #f
-  (or (p? (car list)) (any? p? (cdr list)))))
+  (or (predicate? (car list)) (any? predicate? (cdr list)))))
 
-(define (every? p? list)
+(test (any? number? '(5 6 4 3 3)) #t) 
+(test (any? number? '(g d 2 c x s)) #t) 
+(test (any? number? '(d f c !)) #f) 
+(test (any? symbol? '(v y 34d c)) #t) 
+(test (any? symbol? '(4 4 4 4)) #f) 
+
+(define (every? predicate? list)
   (if (empty? list) #t
-  (and (p? (car list)) (any? p? (cdr list)))))
+  (and (predicate? (car list)) (every? predicate? (cdr list)))))
 
+(test (every? empty? '(5 6 4 3 3)) #f) 
+(test (every? number? '(5 6 4 3 4 5 3)) #t) 
+(test (every? number? '(d fc 3 4)) #f) 
+(test (every? symbol? '(df cf 344d cr)) #t) 
+(test (every? symbol? '(4 4 4 4)) #f) 
